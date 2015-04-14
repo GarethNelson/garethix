07277a6fceb4d798a6366e8d6fb4cc0d9a779b2a
GarethNelson <gareth@garethnelson.com>
2015-04-14 09:06:24 +0100

    Dump memory map in a sane format

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -13,10 +13,15 @@ void kmain(struct multiboot_info *mboot_ptr, uint32_t initial_stack) {
      kprintf("Booting garethix, SVN version %s...\n", KERNEL_SVN_REV);
      kprintf("Build: %s, Build host: %s\n", BUILDTIME, BUILDHOST);
      kprintf("command line: %s\n",(char*)mboot_ptr->cmdline);
-     kprintf("Memory map length: %d\n", mboot_ptr->mmap_length);
+     kprintf("Dumping memory map:\n");
      memory_map_t* mmap = mboot_ptr->mmap_addr;
      while(mmap < mboot_ptr->mmap_addr + mboot_ptr->mmap_length) {
-                kprintf("%d, %d kb, type: %d\n", mmap->base_addr_low, mmap->length_low/1024, mmap->type);
+                kprintf("0x%x, %d kb", mmap->base_addr_low, mmap->length_low/1024);
+                if (mmap->type==1) { 
+                   kprintf(" FREE\n");
+                } else {
+                   kprintf(" RESERVED, type %d\n", mmap->type);
+                }
 		mmap = (memory_map_t*) ( (unsigned int)mmap + mmap->size + sizeof(unsigned int) );
      }
 #if defined(__i386__)
