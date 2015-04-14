bc831689ad59fe4a7afae1466a2021937bf518c8
GarethNelson <gareth@garethnelson.com>
2015-04-14 06:27:14 +0100

    Scrap floppy image

--- build.sh
+++ build.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
+
 pushd src/kernel/
 make clean
 make
 popd
+
 pushd initrd/
 make clean
 make
 popd
-sudo mount floppy.img floppy
-sudo rm floppy/kernel
-sudo rm floppy/initrd
-sudo cp src/kernel/kernel floppy/
-sudo cp initrd/initrd.img floppy/initrd
-sudo umount floppy
+
 
