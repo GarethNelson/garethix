c301fdfaf483d559a0e39fc250b4f01636127f7f
Gareth Nelson <gareth@localhost.localdomain>
2015-04-14 05:47:19 +0100

    import toolchain

---
+++ toolchain/build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+mkdir $HOME/opt/cross
+
+rm -rf build/binutils/*
+rm -rf build/gcc-4.7.2/*
+
+export PREFIX="$HOME/opt/cross"
+export TARGET=i686-elf
+export PATH="$PREFIX/bin:$PATH"
+
+pushd build/binutils
+../binutils-2.22/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+make
+make install
+popd
+
+pushd build/gcc
+../gcc-4.7.2/configure  --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
+make all-gcc
+make all-target-libgcc
+make install-gcc
+make install-target-libgcc
+popd
